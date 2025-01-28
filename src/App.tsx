ffaa1f99e4362caaba4feb2588a4306268813d59
Alex Ivy-Townley <alextownleymakes@gmail.com>
2025-01-28 12:59:53 -0600

    nav, sign up, sign in

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AgencyList from "./pages/AgencyList.tsx";
 import Home from "./pages/Home.tsx";
+import SignIn from "./pages/SignIn.tsx";
+import SignUp from "./pages/SignUp.tsx";
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/agency-list" element={<AgencyList />} />
                 <Route path="/" element={<Home />} />
+                <Route path="/agency-list" element={<AgencyList />} />
+                <Route path="/sign-in" element={<SignIn/>} />
+                <Route path="/sign-up" element={<SignUp/>} />
             </Routes>
         </Router>
     );
